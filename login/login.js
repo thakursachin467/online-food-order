3f0d2443a47cb787fef6d44da6f5413f1ac09a34
thakursachin467 <thakursachin467@gmail.com>
2018-03-02 03:08:28 +0530

    late

--- login/login.js
+++ login/login.js
@@ -8,6 +8,8 @@
   };
   firebase.initializeApp(config);
   var provider = new firebase.auth.GoogleAuthProvider();
+  var provider1 = new firebase.auth.FacebookAuthProvider();
+  var provider2 = new firebase.auth.TwitterAuthProvider();
 
 
 
@@ -112,6 +114,45 @@ firebase.auth().signInWithRedirect(provider).then(function(result) {
   // ...
 });
 
+}
+
+
+function facebooksignin() {
+  firebase.auth().signInWithPopup(provider1).then(function(result) {
+  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+}
+
+function twittersignin() {
+  firebase.auth().signInWithPopup(provider2).then(function(result) {
+  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
 }
 
 
