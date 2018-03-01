12c3b4903ac593684805edafcb8731cc70a45515
thakursachin467 <thakursachin467@gmail.com>
2018-03-02 03:14:23 +0530

    ahh

--- login/login.js
+++ login/login.js
@@ -137,9 +137,11 @@ function facebooksignin() {
 }
 
 function twittersignin() {
-  firebase.auth().signInWithPopup(provider2).then(function(result) {
-  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
+  // You can use these server side with your app's credentials to access the Twitter API.
   var token = result.credential.accessToken;
+  var secret = result.credential.secret;
   // The signed-in user info.
   var user = result.user;
   // ...
