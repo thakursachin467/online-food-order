57d38daba1e4bb61974a4e3bb375a5fc4ebb7bd9
thakursachin467 <thakursachin467@gmail.com>
2018-03-03 14:36:21 +0530

    changes

--- index.js
+++ index.js
@@ -8,3 +8,25 @@ $(window).scroll(function(){
        $('.firstnav').css("visibility","visible");
     }
 });
+
+
+
+$(document).ready(function(){
+
+	$('.image-2').mouseover(function(){
+
+		$(this).css("border", "2px solid red");
+		$(this).css("box-shadow", "6px 9px #808080");
+
+
+	});
+
+	$('.image-2').mouseout(function(){
+
+		$(this).css("border", "2px solid black");
+		$(this).css("box-shadow", "2px 3px #808080");
+	});
+
+
+
+});
